8992db4b960783ce94ea510cb8128d29b6c91737
Sora251135 <arnovdb3@gmail.com>
2025-08-20 15:42:30 +0200

    Template added

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+let score = 100;
+if (score = 0) {
+  console.log("You lost.");
+}
+
+
+  let x = 5;
+x += 2;
+console.log(x);
+
+var name;
+name = "Ava";
+console.log(name);
+
+let nums = [1, 2, 3];
+console.log(nums.length);
+
+let a = 3 + "3";
+console.log(a);
+
+
+
+var myVar
