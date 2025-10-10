8a0c81619591246aaa10b684b69e26072a02c97a
Sora251135 <arnovdb3@gmail.com>
2025-10-10 12:28:30 +0200

    Finished SignUp/SignIn page
    Added some styling
    still need to make the code capture the users name for it to be displayed on the home page

--- js/script.js
+++ js/script.js
@@ -1,77 +1,34 @@
-class Movie{
-  constructor(image, year, title, description, rating, link){
-    this.image = image;
-    this.year = year;
-    this.title = title;
-    this.description = description;
-    this.rating = rating;
-    this.link = link;
-  }
-}
+//===================================================================================//
 
-function isHightRated(movie){
-  return movie.rating >= 7.4;
-}
+//Start of Sign Up and Sign in page script.js
 
-!async function() {
+// Toggle between Sign In and Sign Up forms using jQuery black magic
+$(function(){
+  var $signIn  = $('#sign-in-form'),
+      $signUp  = $('#sign-up-form'),
 
-  const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-  const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NmVhMjI0YjE0NjU1Y2MyYWJjM2U3MjFhZTQzMzU4NSIsIm5iZiI6MTc1ODExODk0NC41NzIsInN1YiI6IjY4Y2FjNDIwOThmZmQ1NDU1MGUzNjQ5YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EB1Jw38BBXuv-nT5v9wJYKatVdvxHNY8LMWUar0LAGY'
-  }
-};
+      $toggleText = $('#toggle-text');
 
-  let data = await fetch(url, options)
-      .then ((response)=> response.json())
-      .then ((results)=> {return results})
-      .catch ((error)=> console.log(error));
+  $toggleText.on('click', '#toggle-link', function(stopPageReset){
+    stopPageReset.preventDefault();
+    $signIn.toggle();
+    $signUp.toggle();
 
-  console.log(data);
+    //does the text toggle 
+    if ($signIn.is(':visible')) {
+      $toggleText.html(
+        'Don’t have an account? ' +
+        '<a href="#" id="toggle-link">Sign up</a>'
+      );
+    } else {
+      $toggleText.html(
+        'Already have an account? ' +
+        '<a href="#" id="toggle-link">Sign in</a>'
+      );
+    }
+  });
+});
 
+//end of Sign in and Sign up page script.js
 
-
-  let movieList = [];
-
-  for (i = 0; i < data.results.length; i++) {
-
-    let image = data.results[i].poster_path;
-    let title = data.results[i].title;
-    let year = data.results[i].release_date;
-    let description = data.results[i].overview;
-    let rating = data.results[i].vote_average;
-    let link = `https://www.themoviedb.org/movie/${data.results[i].id}`;
-
-    movieList.push(window["movie_" + i] = new Movie(image, year, title, description, rating, link));
-
-  }
-
-
-console.log(movieList);
-
-let bestMovies = movieList.filter(isHightRated);
-
-console.log(bestMovies);
-
-bestMovies.forEach(movie => {
-  document.getElementById('CardBox').innerHTML += ` <div class="col-md-4">
-
-            <div class="card">
-              <img src="${movie.image}" class="card-img-top" alt="..." style="height: 400px; object-fit: cover;">
-              <div class="card-body">
-                <h5 class="card-title">${movie.title}</h5>
-                <p class="card-text">${movie.description}</p>
-                <div class="d-flex gap-2">
-                  <button class="btn btn-danger btn-sm">Watch</button>
-                  <button class="btn btn-dark btn-sm">Add to list</button>
-                </div>
-              </div>
-            </div>
-
-  </div>
-  `
-})
-
-}();
+//===================================================================================//
